a5d9b7f8c4cb7812816d807a6c91110deb7dd5b5
Carola <carocastabecq@gmail.com>
2025-07-28 19:06:49 -0300

    Archivos agregados

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
 
 
 
-prompt("¿Cuàntos dias a la semana hacer ejercicio?")
+ejerciciosSemana = prompt("¿Cuàntos dias a la semana sueles hacer ejercicio?")
+
+alert("La cantidad de dias que haces ejercicios son: " + ejerciciosSemana)
+
+//agregar condicion con if, si es menos q 3 no es saludable + mensaje
+//si quiere ingresar un dato q no sea numero ERROR
+
+
+horasSuenio = prompt("¿Cuántas horas dormís por noche en promedio?")
+
+alert("El promedio de horas dormidas son: " + horasSuenio)
+
+//agregar condicion con if, si es menos q 8 no es saludable + mensaje
+//si quiere ingresar un dato q no sea numero ERROR
+
+tomarAgua = prompt("¿Cuantos litros de agua sueles tomar por dìa?")
+
+alert("La cantidad de litros de agua son: " + tomarAgua)
+
+
+usoDispositivos = prompt("¿Cuántas horas por día pasás frente a una pantalla (celular, compu, etc.)?")
+
+alert("La cantidad de horas de uso del celular son: " + usoDispositivos)
+
+
+fumadores = prompt("¿Fumás cigarrillos o vapeás regularmente?")
+
+
+
+tomarAlcohol = prompt("¿Tomás bebidas alcohólicas más de 2 veces por semana?")
+
+
+
+consumoFrutas = prompt("¿Cuántas porciones de frutas y verduras consumís por día?")
+
+alert("La cantidad de frutas y verduras consumidas son: " + consumoFrutas)
